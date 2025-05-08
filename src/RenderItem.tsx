a9d11cd94e8b22a722b473baee835209e82bdd83
AronJustoC <aronchoque727@gmail.com>
2025-05-08 10:27:09 -0500

    feat: basic project React

--- src/RenderItem.tsx
+++
@@ -1,9 +0,0 @@
-import { memo } from "react";
-import { IClient } from "./App";
-
-const RenderItem = ({ client }: { client: IClient }) => {
-  console.log("re renderizado");
-  return <ul>{client.name}</ul>;
-};
-
-export default memo(RenderItem);
